eafaa43e9299cc8f7f8c6194fed0448d2e994672
Rob Leonard <rleonard55@gmail.com>
2017-04-17 16:47:57 -0500

    added subscription cleanups now can use presence sensors to figure out arm mode allows hello home phrases

--- smartapps/rleonard55/sync-shm-and-switch.src/sync-shm-and-switch.groovy
+++ smartapps/rleonard55/sync-shm-and-switch.src/sync-shm-and-switch.groovy
@@ -24,9 +24,23 @@ definition(
     iconX3Url: "https://cdn4.iconfinder.com/data/icons/materia-security-vol-3/24/022_110_crypto_switch_commutator_security_lock_arrows-256.png")
 
 preferences {
-	section("Title") {
-		input("MySwitch", "capability.switch", title: "Switches to Sync", multiple: false, required: true)
-	}
+	page(name:"SettingsPage")
+}
+
+def SettingsPage(){
+	dynamicPage(name:"SettingsPage", install:true, uninstall:true){
+        section("Sync SHM and Switch") {
+            input("MySwitch", "capability.switch", title: "Switches to Sync", multiple: false, required: true)
+            input ("presenceSensors", "capability.presenceSensor", title: "Presence Sensor(s)", multiple: true, required: true)
+            def phrases = location.helloHome?.getPhrases()*.label
+            if (phrases) {
+                phrases.sort()
+                input (name:"armRoutine",type: "enum", title: "Arm Away Routine", options: phrases, required:false)
+                input (name:"stayRoutine",type: "enum", title: "Stay Routine", options: phrases, required:false)
+                input (name:"disarmRoutine",type: "enum", title: "Disarm Routine", options: phrases, required:false)
+            }
+        }
+    }
 }
 
 def installed() {
@@ -34,46 +48,66 @@ def installed() {
 
 	initialize()
 }
-
 def updated() {
 	log.debug "Updated with settings: ${settings}"
 
 	unsubscribe()
 	initialize()
 }
-
 def initialize() {
-	 subscribe(location,"alarmSystemStatus",alarmStatusHandler)
+	subscribeIt()
+}
+
+def subscribeIt(){
+    subscribe(location,"alarmSystemStatus",alarmStatusHandler)
      subscribe(MySwitch, "switch.on", switchOnHandler)
      subscribe(MySwitch, "switch.off", switchOffHandler)
 }
 
 def alarmStatusHandler(event) {
 	log.debug "Caught alarm status change: "+event.value
+    unsubscribe()
+    
     if (event.value == "off") MySwitch.off()
     else if (event.value == "away")  MySwitch.on()
     else if (event.value == "stay") MySwitch.on()
+    
+    subscribeIt()
 }
 def switchOnHandler (evt) {
-	log.debug "Switch on handler raised: ${evt}"
+	log.debug "Switch on handler raised"
+    unsubscribe()
+    
     def armMode = 'stay'
+    if(!anyonePresent())
+    	armMode = 'away'
+    
 	sendSHMEvent(armMode)
 	execRoutine(armMode)
+    
+	subscribeIt()
 }
 def switchOffHandler (evt) {
-	log.debug "Switch off handler raised: ${evt}"
+	log.debug "Switch off handler raised"
     def armMode = 'off'
+    unsubscribe()
+     
 	sendSHMEvent(armMode)
 	execRoutine(armMode)
+    
+    subscribeIt()
 }
+
 private sendSHMEvent(String shmState){
 	def event = [name:"alarmSystemStatus", value: shmState, 
     			displayed: true, description: "System Status is ${shmState}"]
     sendLocationEvent(event)
 }
-
 private execRoutine(armMode) {
 	if (armMode == 'away') location.helloHome?.execute(settings.armRoutine)
     else if (armMode == 'stay') location.helloHome?.execute(settings.stayRoutine)
     else if (armMode == 'off') location.helloHome?.execute(settings.disarmRoutine)    
+}
+private anyonePresent() {
+	return presenceSensors.any {p -> p.currentValue("presence") == "present" }
 }
